e823c3703bcd3dd4c41ad4dcd9432456da1377d5
Sammy Schneider <SammySchneider@Sammys-MacBook-Air.local>
2021-02-17 12:52:05 -0700

    add allowed hosts

--- practice/settings.py
+++ practice/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!pz(m&ccg(kj7w_d2c_!39fr^k960fqve!kfq1qn)cz)2ug^@j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://morning-basin-77217.herokuapp.com' ,'127.0.0.1' ]
 
 
 # Application definition
